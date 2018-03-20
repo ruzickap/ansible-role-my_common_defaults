bb72a093cc221a5a13b87c8b7900a4146b879369
Petr Ruzicka <petr.ruzicka@gmail.com>
2018-03-20 14:10:21 +0100

    Windows additions

--- tests/vagrant_test.sh
+++ tests/vagrant_test.sh
@@ -3,23 +3,28 @@
 TEMP_PATH="/tmp/mytest/"
 VAGRANT_BOXES="peru/windows-server-2016-standard-x64-eval peru/windows-10-enterprise-x64-eval peru/windows-server-2012-r2-standard-x64-eval"
 
-IP_ADDRESSES=""
+HOSTS=""
 for VAGRANT_BOX in $VAGRANT_BOXES; do
   BOX="${VAGRANT_BOX##*/}"
   echo "*** $VAGRANT_BOX : $BOX"
 
-  test -d "$TEMP_PATH/$BOX" || mkdir -v -p "$TEMP_PATH/$BOX"
-  cd "$TEMP_PATH/$BOX"
-  test -f Vagrantfile || vagrant init $VAGRANT_BOX
-  VAGRANT_DEFAULT_PROVIDER=libvirt vagrant up
-  IP_ADDRESSES="`vagrant winrm-config | awk '/HostName/ { print $2 }'`,$IP_ADDRESSES"
-  cd -
+  HOSTS="$HOSTS,$BOX"
+
+  if [ ! -f $TEMP_PATH/$BOX/Vagrantfile ]; then
+    test -d "$TEMP_PATH/$BOX" || mkdir -v -p "$TEMP_PATH/$BOX"
+    cd "$TEMP_PATH/$BOX"
+    vagrant init $VAGRANT_BOX
+    sed -i "/^Vagrant\.configure/a \ \ config.hostmanager.enabled = true\n \ config.hostmanager.manage_host = true\n \ config.hostmanager.aliases = \"$BOX\"" Vagrantfile
+    VAGRANT_DEFAULT_PROVIDER=libvirt vagrant up
+    cd -
+  else
+    echo "*** $TEMP_PATH/$BOX/Vagrantfile exists... skipping provisioning"
+  fi
 done
 
-ansible-playbook --extra-vars "ansible_user=vagrant ansible_password=vagrant ansible_port=5986 ansible_connection=winrm ansible_winrm_server_cert_validation=ignore" -i "$IP_ADDRESSES" ./test.yml
-ansible-playbook --extra-vars "ansible_user=Administrator ansible_password=vagrant ansible_port=5986 ansible_connection=winrm ansible_winrm_server_cert_validation=ignore" -i "$IP_ADDRESSES" ./test.yml
+ansible-playbook --extra-vars "ansible_user=Administrator ansible_password=vagrant ansible_port=5986 ansible_connection=winrm ansible_winrm_server_cert_validation=ignore" -i "$HOSTS," ./test.yml
 
-echo "Press ENTER to Destroy all VMs"
+echo "Press ENTER to Destroy all VMs (or CRTL+C to cancel)"
 read A
 
 for VAGRANT_BOX in $VAGRANT_BOXES; do

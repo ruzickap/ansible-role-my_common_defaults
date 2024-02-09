7d37dca53d0dc15619c47dd61fec639b0352772d
Petr Ruzicka <petr.ruzicka@gmail.com>
2024-02-09 06:26:37 +0100

    feat(gha): unify GHA - renovate, megalinter, markdown, and others

--- tests/vagrant_test_windows.sh
+++ tests/vagrant_test_windows.sh
@@ -16,28 +16,28 @@ for VAGRANT_BOX in $VAGRANT_BOXES; do
 
     docker pull peru/vagrant_libvirt_virtualbox
     docker run --rm -it -u "$(id -u):$(id -g)" --privileged --net=host \
-    -e HOME=/home/docker \
-    -e VAGRANT_DEFAULT_PROVIDER=libvirt \
-    -e ANSIBLE_HOST_KEY_CHECKING=False \
-    -v /dev/vboxdrv:/dev/vboxdrv \
-    -v /var/run/libvirt/libvirt-sock:/var/run/libvirt/libvirt-sock \
-    -v "$TEMP_PATH/$BOX":/home/docker/vagrant \
-    -v "$ANSIBLE_ROLE_DIR":/home/docker/ansible_role \
-    -v "$HOME/.vagrant.d/boxes":/home/docker/.vagrant.d/boxes \
-    peru/vagrant_libvirt_virtualbox "\
-    set -ux \
-    && vagrant init $VAGRANT_BOX \
-    && vagrant up \
-    && VAGRANT_HOST=\`vagrant ssh-config | awk '/HostName / { print \$2 }'\` \
-    && ansible-playbook --extra-vars \"ansible_user=Administrator ansible_password=vagrant ansible_connection=winrm ansible_winrm_server_cert_validation=ignore\" -i \$VAGRANT_HOST, \$HOME/ansible_role/tests/test.yml \
-    && ansible-playbook --extra-vars \"ansible_user=Administrator ansible_password=vagrant ansible_connection=winrm ansible_winrm_server_cert_validation=ignore\" -i \$VAGRANT_HOST, \$HOME/ansible_role/tests/test.yml \
-    && bash \
-    ;  vagrant destroy -f \
-    "
+      -e HOME=/home/docker \
+      -e VAGRANT_DEFAULT_PROVIDER=libvirt \
+      -e ANSIBLE_HOST_KEY_CHECKING=False \
+      -v /dev/vboxdrv:/dev/vboxdrv \
+      -v /var/run/libvirt/libvirt-sock:/var/run/libvirt/libvirt-sock \
+      -v "$TEMP_PATH/$BOX":/home/docker/vagrant \
+      -v "$ANSIBLE_ROLE_DIR":/home/docker/ansible_role \
+      -v "$HOME/.vagrant.d/boxes":/home/docker/.vagrant.d/boxes \
+      peru/vagrant_libvirt_virtualbox "\
+      set -ux \
+      && vagrant init $VAGRANT_BOX \
+      && vagrant up \
+      && VAGRANT_HOST=\`vagrant ssh-config | awk '/HostName / { print \$2 }'\` \
+      && ansible-playbook --extra-vars \"ansible_user=Administrator ansible_password=vagrant ansible_connection=winrm ansible_winrm_server_cert_validation=ignore\" -i \$VAGRANT_HOST, \$HOME/ansible_role/tests/test.yml \
+      && ansible-playbook --extra-vars \"ansible_user=Administrator ansible_password=vagrant ansible_connection=winrm ansible_winrm_server_cert_validation=ignore\" -i \$VAGRANT_HOST, \$HOME/ansible_role/tests/test.yml \
+      && bash \
+      ;  vagrant destroy -f \
+      "
 
-  echo "*** Removing $VAGRANT_BOX : $BOX: $TEMP_PATH/$BOX"
-  rm -rf "$TEMP_PATH/$BOX/"{Vagrantfile,.vagrant}
-  rmdir "$TEMP_PATH/$BOX"
+    echo "*** Removing $VAGRANT_BOX : $BOX: $TEMP_PATH/$BOX"
+    rm -rf "$TEMP_PATH/$BOX/"{Vagrantfile,.vagrant}
+    rmdir "$TEMP_PATH/$BOX"
 
   else
     echo "*** $TEMP_PATH/$BOX/Vagrantfile exists... skipping provisioning"
